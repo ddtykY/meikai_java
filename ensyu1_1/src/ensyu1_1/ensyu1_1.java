d37cb43bee6abbd34f6fcda516f02713e069d548
山川勇輝 <a@ddt.com>
2023-04-04 16:51:15 +0900

    演習1-1の内容を変更しました。

--- ensyu1_1/src/ensyu1_1/ensyu1_1.java
+++ ensyu1_1/src/ensyu1_1/ensyu1_1.java
@@ -1,8 +1,18 @@
 package ensyu1_1;
+/**
+ * このクラスは演習1-1の回答のクラスです。
+ * 
+ * @author 山川勇輝
+ */
 
 public class ensyu1_1 {
+	
+	/**
+	 * 演習1-1のメインメソッドです。
+	 * ;が欠如している際の動きを検証するためにどちらの文からも;を省いています。
+	 */
 	public static void main(String[] args) {
-	    //System.out.println("初めてのjavaプログラミング")
-	    //System.out.println("画面に出力しています")
+	    System.out.println("初めてのjavaプログラミング")
+	    System.out.println("画面に出力しています")
 	}
 }
