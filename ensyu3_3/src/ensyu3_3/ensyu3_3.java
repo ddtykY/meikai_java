e120a0ed75cf4fca27ae15da6bab8939c0540f46
山川勇輝 <a@ddt.com>
2023-04-05 18:45:06 +0900

    演習問題3-3の回答を作成しました。 変更があった場合でも特に動作が変わることはないと考えます。

---
+++ ensyu3_3/src/ensyu3_3/ensyu3_3.java
@@ -0,0 +1,46 @@
+package ensyu3_3;
+
+//Scannerクラスを用いるためにimportする
+import java.util.Scanner;
+
+/*
+* クラス名:ensyu3_3
+* 概要    :演習3-3の回答クラスです。
+* 作成者  :山川勇輝
+* 作成日  :2023.04.05
+*/
+
+public class ensyu3_3 {
+	/**
+	 * 関数名           :main
+	 * メソッドの説明   :教本List3-5のelseをelse if (n == 0)に変更した結果を検討するためのもの
+	 *                  :List3-5は読み込んだ整数値符号（正/負/0）を判定して表示するメソッド
+	 * パラメータの説明 :なし
+	 * 返り値           :なし
+	 * 作成者           :山川勇輝
+	 * 作成日           :2023.04.05
+	 */
+	public static void main(String[] args) {
+		// Scannerクラスのインスタンスを作成する
+		Scanner standardInput = new Scanner(System.in);
+		
+		// 整数値"value"の入力を促す
+		System.out.print("整数value : ");
+		// int型の変数"value"に入力された整数値を代入する
+		int value = standardInput.nextInt();
+		
+		if (value > 0) {
+			// "value"が0より大きいなら正である旨を表示する
+			System.out.println("その値は正です");
+		} else if (value < 0){
+			// "value"が0より小さいなら負である旨を表示する
+			System.out.println("その値は負です");
+		} else if (value == 0) {
+			// "value"が0であるならその旨を表示する
+			System.out.println("その値は0です");
+		}
+		
+		//standardInputを閉じる
+		standardInput.close();
+	}
+}
