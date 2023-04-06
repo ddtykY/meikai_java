73a5ab7b1fbde7d8a789623dd55ee8f7b9df6a3d
山川勇輝 <a@ddt.com>
2023-04-06 11:15:00 +0900

    演習問題3-10の回答を作成しました。

---
+++ ensyu3_10/src/ensyu3_10/ensyu3_10.java
@@ -0,0 +1,44 @@
+package ensyu3_10;
+
+//Scannerクラスを用いるためにimportする
+import java.util.Scanner;
+
+/*
+* クラス名:ensyu3_10
+* 概要    :演習3-10の回答クラスです。
+* 作成者  :山川勇輝
+* 作成日  :2023.04.06
+*/
+public class ensyu3_10 {
+	/**
+	 * 関数名           :main
+	 * メソッドの説明   :二つの整数値を読み込んでそれらの差を表示する
+	 * パラメータの説明 :なし
+	 * 返り値           :なし
+	 * 作成者           :山川勇輝
+	 * 作成日           :2023.04.06
+	 */
+	public static void main(String[] args) {
+		// Scannerクラスのインスタンスを作成する
+		Scanner standardInput = new Scanner(System.in);
+		
+		// 整数値"firstValue"の入力を促す
+		System.out.print("実数値firstValue : ");
+		// int型の変数"firstValue"に入力された整数値を代入する
+		int firstValue = standardInput.nextInt();
+		
+		// 整数値"secondValue"の入力を促す
+		System.out.print("実数値secondValue : ");
+		// int型の変数"secondValue"に入力された整数値を代入する
+		int secondValue = standardInput.nextInt();
+		
+		// 条件演算子を用いて二つの数値の差を求めてint型の変数"difference"
+		int difference = firstValue > secondValue ? firstValue - secondValue : secondValue - firstValue;
+		
+		// 二値の差を表す変数"difference"を表示する
+		System.out.println("二つの整数値の差は " + difference + " です。");
+		
+		//standardInputを閉じる
+		standardInput.close();
+	}
+}
