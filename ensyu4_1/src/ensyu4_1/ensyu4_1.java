67b375914b1bf34289c60378ba55829626dfb34f
山川勇輝 <a@ddt.com>
2023-04-06 16:11:09 +0900

    演習問題4-1の回答を作成しました。

---
+++ ensyu4_1/src/ensyu4_1/ensyu4_1.java
@@ -0,0 +1,49 @@
+package ensyu4_1;
+
+//Scannerクラスを用いるためにimportする
+import java.util.Scanner;
+
+/*
+ * クラス名:ensyu4_1
+ * 概要    :演習問題4_1の回答クラスです。
+ * 作成者  :山川勇輝
+ * 作成日  :2023.04.06
+ */
+public class ensyu4_1 {
+	/*
+	 * 関数名           :main
+	 * メソッドの説明   :読み込んだ整数値の符号を判定して表示（何度も繰り返すことができる）
+	 * パラメータの説明 :なし
+	 * 返り値           :なし
+	 * 作成者           :山川勇輝
+	 * 作成日           :2023.04.06
+	 */
+	public static void main(String[] args) {
+		// Scannerクラスのインスタンスを作成
+		Scanner standardInput = new Scanner(System.in);
+		
+		int retry; // もう一度行うかどうかの確認用
+		
+		do {
+			// 判定したい整数値の入力を促す
+			System.out.print("整数値 : ");
+			// int型の変数"value"に入力された整数値を代入する
+			int value = standardInput.nextInt();
+			
+			if (value > 0) // "value"が0より大きければ
+				System.out.println("その値は正です。"); // 正の整数値であるという旨を表示する
+			else if (value < 0) // "value"が0より小さければ
+				System.out.println("その値は負です。"); // 負の整数値であるという旨を表示する
+			else  // ほかのどの条件にも当てはまらなければ（valueが0ならば）
+				System.out.println("その値は0です。"); // 0であるという旨を表示する
+			
+			// もう一度行うかどうかを確認するための整数値の入力を促す
+			System.out.print("もう一度？ 1…Yes / 0…No : ");
+			// 確認用のint型の変数"retry"に入力された値を代入する
+			retry = standardInput.nextInt();
+		} while (retry == 1);  // "retry"が1ならば繰り返す
+		
+		//standardInputを閉じる
+		standardInput.close();
+	}
+}
