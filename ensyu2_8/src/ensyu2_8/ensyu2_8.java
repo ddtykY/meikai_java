9ff816e861d65e011008770738415c849d462b8b
山川勇輝 <a@ddt.com>
2023-04-07 10:12:20 +0900

    2-8の内容を編集しました。

--- ensyu2_8/src/ensyu2_8/ensyu2_8.java
+++ ensyu2_8/src/ensyu2_8/ensyu2_8.java
@@ -6,8 +6,8 @@ import java.util.Random;
 import java.util.Scanner;
 
 /*
- * クラス名:ensyu2_7
- * 概要    :演習2-7の回答クラスです。
+ * クラス名:ensyu2_8
+ * 概要    :演習2-8の回答クラスです。
  * 作成者  :山川勇輝
  * 作成日  :2023.04.05
  */
@@ -24,22 +24,22 @@ public class ensyu2_8 {
 		// Scannerクラスのインスタンスを作成する
 		Scanner standardInput = new Scanner(System.in);
 		// Randomクラスのインスタンスを作成する
-		Random rand = new Random();
+		Random randomNumber = new Random();
 		
-		// 整数値"Value"の入力を促す
+		// 整数値"integerValue"の入力を促す
 		System.out.print("整数値 : ");
-		// int型の変数"value"に整数値を読み込む
-		int value = standardInput.nextInt();
+		// int型の変数"integerValue"に整数値を読み込む
+		int integerValue = standardInput.nextInt();
 		
 		// "value"の値に5を加算し、そこから0～10のランダムな数を減算、
-		// それにより最大値が"value"+5、最小値が"value"-5であるような数を生成、
-		// それをint型の変数"valuePlusMinus5"に代入する
-		int valuePlusMinus5 = value + 5 - rand.nextInt(11);
+		// それにより最大値が"integerValue"+5、最小値が"integerValue"-5であるような数を生成、
+		// それをint型の変数"plusMinus5"に代入する
+		int plusMinus5 = integerValue + 5 - randomNumber.nextInt(11);
 		
 		// 入力された整数値の±5の乱数を生成した旨を表示する
 		System.out.println("その値の±5の乱数を生成しました。");
-		// "valuePlusMinus5"の値を表示する
-		System.out.println("それは" + valuePlusMinus5 + "です。");
+		// "plusMinus5"の値を表示する
+		System.out.println("それは" + plusMinus5 + "です。");
 		
 		// standardInputを閉じる
 		standardInput.close();
