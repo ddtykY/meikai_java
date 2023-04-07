ef76757ef41b1a6c673a424d0b7e96471f17a484
山川勇輝 <a@ddt.com>
2023-04-07 10:17:18 +0900

    3-5の内容を編集しました。

--- ensyu3_5/src/ensyu3_5/ensyu3_5.java
+++ ensyu3_5/src/ensyu3_5/ensyu3_5.java
@@ -22,16 +22,16 @@ public class ensyu3_5 {
 		// Scannerクラスのインスタンスを作成する
 		Scanner standardInput = new Scanner(System.in);
 		
-		// 実数値"firstValue"の入力を促す
+		// 実数値"integerValue"の入力を促す
 		System.out.print("正の整数value : ");
-		// int型の変数"value"に入力された整数値を代入する
-		int value = standardInput.nextInt();
+		// int型の変数"integerValue"に入力された整数値を代入する
+		int integerValue = standardInput.nextInt();
 		
 		
-		if (value <= 0) {
+		if (integerValue <= 0) {
 			// 正でない値が入力された場合その旨を表示する
 			System.out.println("正でない値が入力されました。");
-		} else if (value % 5 == 0){
+		} else if (integerValue % 5 == 0){
 			// "value"が5で割り切れる場合はその旨を表示する
 			System.out.println("その値は5で割り切れます。");
 		} else {
