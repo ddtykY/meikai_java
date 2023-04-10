79766cc99a9ba973bb050a17d271910cb29f93ea
山川勇輝 <a@ddt.com>
2023-04-10 16:03:08 +0900

    演習問題25の内容を編集し、またもう一つのクラスのファイルをコミットし忘れていたため追加します.

--- ensyu4_25/src/ensyu4_25/ensyu4_25_2.java
+++ ensyu4_25/src/ensyu4_25/ensyu4_25_2.java
@@ -3,15 +3,15 @@ package ensyu4_25;
 import java.util.Scanner;
 
 /*
- *  クラス名 :ensyu4_25_1
- *  概要     :演習問題4-25のList4-17の解答クラスです。
+ *  クラス名 :ensyu4_25_2
+ *  概要     :演習問題4-25のList4-18の解答クラスです。
  *  作成者   :山川勇輝
  *  作成日   :2023.04.10
  */
-public class ensyu4_25_1 {
+public class ensyu4_25_2 {
 	/*
 	 * 関数名           :main
-	 * メソッドの説明   :読み込んだ整数を加算し平均を求める。このとき0が入力された時点で終了する(List4-17の改変)
+	 * メソッドの説明   :読み込んだ整数を加算し平均を求める。このとき合計が1000を超えない範囲で加算する(List4-18の改変)
 	 * パラメータの説明 :なし
 	 * 返り値           :なし
 	 * 作成者           :山川勇輝
@@ -44,13 +44,17 @@ public class ensyu4_25_1 {
 		// 入力された値"quantiryNumber"回繰り返す
 		for(int countNumber = 0; countNumber < quantityNumber; countNumber++) {
 			// 整数の入力を促す
-			System.out.print("整数(0で終了) : ");
+			System.out.print("整数: ");
 			// int型の整数"integerValue"を入力された値で初期化する
 			int integerValue = standardInput.nextInt();
 			
-			// もし入力された値が0なら
-			if (integerValue == 0) {
-				// for文から抜け出す
+			// 入力された値を"sumValue"に加算したとき1000を超えるなら
+			if(sumValue + integerValue > 1000) {
+				// 合計が1000を超えた旨を表示する
+				System.out.println("合計が1000を超えました");
+				// 最後の数値を無視することを伝える
+				System.out.println("最後の数値は無視します");
+				// for文を抜け出す
 				break;
 			}
 			// 合計を表す変数"sumValue"に入力された値(integeValue)を加算する
@@ -59,15 +63,7 @@ public class ensyu4_25_1 {
 			sumCount++;
 		}
 		
-		// "sumCount"がなら平均を求めることができないため
-		// もし"sumCount"が0の場合
-		if (sumCount == 0) {
-			// "sumCount"を1として考えることとし1を代入する
-			sumCount = 1;
-		}		
 		// 合計と平均を表示する
-		System.out.print("合計は " + sumValue + " で平均は " + sumValue / sumCount + "です。");
-		
-		
+		System.out.print("合計は " + sumValue + " で平均は " + sumValue / sumCount + " です。");
 	}
 }
