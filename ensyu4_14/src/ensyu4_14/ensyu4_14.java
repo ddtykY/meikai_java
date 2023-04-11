76a0084f29646f1583b22604b703b0bf5e8a4aec
山川勇輝 <a@ddt.com>
2023-04-11 13:10:42 +0900

    4-14のコメントを編集しました。

--- ensyu4_14/src/ensyu4_14/ensyu4_14.java
+++ ensyu4_14/src/ensyu4_14/ensyu4_14.java
@@ -20,37 +20,41 @@ public class ensyu4_14 {
 	public static void main(String[] args) {
 		// Scannerクラスのインスタンスを作成
 		Scanner standardInput = new Scanner(System.in);
-		
+
 		// 1から入力された値までの和を求める旨を表示する
 		System.out.println("1から\"integerValue\"までの和を求めます");
-		
+
 		// 入力された値を格納する変数
 		int integerValue;
-		
+
+		// 整数を入力させる処理を正の整数値が入力されるまで繰り返す
 		do {
 			// 正の整数値の入力を促す
 			System.out.print("正の整数値integerValue : ");
 			// 入力された値をint型の変数"integerValue"に代入する
 			integerValue = standardInput.nextInt();
+		// "integerValue"が0以下（正の整数値ではない）間繰り返す
 		} while (integerValue <= 0);
 		
 		// 合計を格納する変数"sumValue"を0で初期化する
 		int sumValue = 0;
-		
+
 		// for文を制御する変数"countNumber"を0で初期化し"integerValue"になるまでインクリメントする
 		for(int countNumber = 1; countNumber <= integerValue; countNumber++) {
 			// 1から入力された値まで増えていく"countNumber"を"sumValue"に加算していく
 			sumValue += countNumber;
 			// 式を表示するために"countNumber"の値を表示する
 			System.out.print(countNumber);
-			
-			if (countNumber != integerValue)  // "countNumber"が"integerValue"と等しくない場合
-				System.out.print(" + ");     // +を表示する
+			// "countNumber"が"integerValue"と等しくない場合
+			if (countNumber != integerValue) {
+				// +を表示する
+				System.out.print(" + ");
+			}
 		}
-			
+
 		// 1から入力された値までの和である"sumValue"の値を表示する
 		System.out.println(" = " + sumValue);
-		
+
 		// 改行文字を出力する
 		System.out.println(); 
 	}
