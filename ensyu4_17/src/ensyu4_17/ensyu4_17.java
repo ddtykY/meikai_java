2eef2ac5eb7f8921c45b2816364e62a8b44aeefb
山川勇輝 <a@ddt.com>
2023-04-11 13:38:01 +0900

    4-17の内容を編集しました。

--- ensyu4_17/src/ensyu4_17/ensyu4_17.java
+++ ensyu4_17/src/ensyu4_17/ensyu4_17.java
@@ -20,24 +20,41 @@ public class ensyu4_17 {
 	public static void main(String[] args) {
 		// Scannerクラスのインスタンスを作成
 		Scanner standardInput = new Scanner(System.in);
-		
+
 		// 整数値の入力を促す
 		System.out.print("整数値 : ");
 		// 入力された整数値をint型の変数"integerValue"に代入する
 		int integerNumber = standardInput.nextInt();
-		
+
 		// 約数の個数を格納する変数を0で初期化する
 		int divisorNumber = 0;
-		
-		// 入力された"integerNumber"回繰り返す、このとき0では数を割ることができないため"countNumber"を1で初期化する
-		for(int countNumber = 1; countNumber <= integerNumber; countNumber++) {
-			if (integerNumber % countNumber == 0) { // "countNumber"で入力された"integerNumber"を割り切れるとき
-				// その数字"countNumber"を表示する
-				System.out.println(countNumber);
-				// 約数の個数を表すint型の変数"divisorNumber"を1つ加算する
-				divisorNumber++;				
+
+		// 入力された値が正の整数なら
+		if (integerNumber > 0) {
+			// 入力された"integerNumber"回繰り返す、このとき0では数を割ることができないため"countNumber"を1で初期化する
+			for(int countNumber = 1; countNumber <= integerNumber; countNumber++) {
+				 // "countNumber"で入力された"integerNumber"を割り切れるとき
+				if (integerNumber % countNumber == 0) {
+					// その数字"countNumber"を表示する
+					System.out.println(countNumber);
+					// 約数の個数を表すint型の変数"divisorNumber"を1つ加算する
+					divisorNumber++;				
+				}
+			}
+		// 入力された値が負の値なら
+		} else if (integerNumber < 0) {
+			// 入力された"-integerNumber"回繰り返す、このとき0では数を割ることができないため"countNumber"を1で初期化する
+			for(int countNumber = -1; countNumber >= integerNumber; countNumber--) {
+				 // "countNumber"で入力された"integerNumber"を割り切れるとき
+				if (integerNumber % countNumber == 0) {
+					// その数字"countNumber"を表示する
+					System.out.println(countNumber);
+					// 約数の個数を表すint型の変数"divisorNumber"を1つ加算する
+					divisorNumber++;				
+				}
 			}
 		}
+
 		// 約数の個数を表示する
 		System.out.println("約数は " + divisorNumber + " 個です。");	
 	}
