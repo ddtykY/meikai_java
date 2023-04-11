f23798131db120107b49ae0130ea50f2d73f7a48
山川勇輝 <a@ddt.com>
2023-04-11 09:44:57 +0900

    演習問題5-3の解答を作成しました。

---
+++ ensyu5_3/src/ensyu5_3/Ensyu5_3.java
@@ -0,0 +1,29 @@
+package ensyu5_3;
+
+/*
+ *  クラス名 :Ensyu5_3
+ *  概要     :演習問題5-3の解答クラスです。
+ *  作成者   :山川勇輝
+ *  作成日   :2023.04.11
+ */
+public class Ensyu5_3 {
+	/*
+	 * 関数名           :main
+	 * メソッドの説明   :
+	 * パラメータの説明 :なし
+	 * 返り値           :なし
+	 * 作成者           :山川勇輝
+	 * 作成日           :2023.04.10
+	 */
+	public static void main(String[] args) {		
+		// 論理型の変数"firstBooleanValue"をtrueで初期化する
+		boolean firstBooleanValue = true;
+		// "firstBooleanValueを表示する"
+		System.out.println(" firstBooleanValue = " + firstBooleanValue);
+
+		// 論理型の変数"secondBooleanValue"をfalseで初期化する
+		boolean secondBooleanValue = false;
+		// "firstBooleanValueを表示する"
+		System.out.println("secondBooleanValue = " + secondBooleanValue);
+	}
+}
