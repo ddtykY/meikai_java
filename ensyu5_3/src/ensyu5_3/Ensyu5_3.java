ae367ae962ca26c10e8b13021da3f2ce34a512a2
山川勇輝 <a@ddt.com>
2023-04-11 11:18:15 +0900

    5-3のメソッドの説明が抜けていたため追加しました

--- ensyu5_3/src/ensyu5_3/Ensyu5_3.java
+++ ensyu5_3/src/ensyu5_3/Ensyu5_3.java
@@ -9,13 +9,13 @@ package ensyu5_3;
 public class Ensyu5_3 {
 	/*
 	 * 関数名           :main
-	 * メソッドの説明   :
+	 * メソッドの説明   :論理型の変数にtrueやfalseを代入してその値を表示する
 	 * パラメータの説明 :なし
 	 * 返り値           :なし
 	 * 作成者           :山川勇輝
 	 * 作成日           :2023.04.10
 	 */
-	public static void main(String[] args) {		
+	public static void main(String[] args) {
 		// 論理型の変数"firstBooleanValue"をtrueで初期化する
 		boolean firstBooleanValue = true;
 		// "firstBooleanValueを表示する"
