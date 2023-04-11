4d749428dd9e09ac5da1e3295a34cda4e3b70135
山川勇輝 <a@ddt.com>
2023-04-11 14:52:15 +0900

    4-24の内容を編集しました。

--- ensyu4_24/src/ensyu4_24/ensyu4_24.java
+++ ensyu4_24/src/ensyu4_24/ensyu4_24.java
@@ -20,14 +20,14 @@ public class ensyu4_24 {
 	public static void main(String[] args) {
 		// Scannerクラスのインスタンスを作成
 		Scanner standardInput = new Scanner(System.in);
-		
+
 		// 素数かどうか判定する旨を知らせる
 		System.out.println("素数かどうか判定します");
-		
+
 		// 入力された正の整数値を格納する変数
 		int integerValue;
-		
-		 // 正の整数値が入力されるまで繰り返す
+
+		// 正の整数値が入力されるまで繰り返す
 		do { 
 			// 2以上の正の整数の入力を促す
 			System.out.print("正の整数(2～): ");
@@ -35,21 +35,23 @@ public class ensyu4_24 {
 			integerValue = standardInput.nextInt();
 		// "integerValue"が1以下である場合繰り返す
 		} while (integerValue <= 1);
-		
+
 		// 素数の判定は2以上"integerValue"未満のいずれの数でも割り切れないかで行うため
-		// for文の制御を行うint型の"countNumber"を2で初期化し,入力された"integerValue-1"まで繰り返す
-		for (int countNumber = 2; countNumber < integerValue; countNumber++) {
+		// for文の制御を行うint型の"countNumber"を2で初期化し,入力された"integerValue"まで繰り返す
+		for (int countNumber = 2; countNumber <= integerValue; countNumber++) {
+			// "countNumber"と"integerValue"が同じ値の場合
+			if (countNumber == integerValue) {
+				// この時点でfor分が中断されていないということは入力された値は素数であるということなのでその旨を表示する
+				System.out.println("その値は素数です");
+				break;
+			}
+			
 			// "integerValue"が"countNumber"で割り切れる場合
 			if (integerValue % countNumber == 0) {
 				// 入力された値が素数ではないことを表示する
 				System.out.println("その値は素数ではありません");
 				// その値が素数でないと分かった時点でfor分を中断する
 				break;
-			}
-			// "countNumber"と"integerValue-1"が同じ値の場合
-			if (countNumber == integerValue - 1) {
-				// この時点でfor分が中断されていないということは入力された値は素数であるということなのでその旨を表示する
-				System.out.println("その値は素数です");
 			}
 		}		
 	}
