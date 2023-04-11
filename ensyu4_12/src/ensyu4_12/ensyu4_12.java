889ac3b160c8743ec4028f89f53d639baaa4836b
山川勇輝 <a@ddt.com>
2023-04-11 13:02:50 +0900

    4-12のコメントを編集しました。

--- ensyu4_12/src/ensyu4_12/ensyu4_12.java
+++ ensyu4_12/src/ensyu4_12/ensyu4_12.java
@@ -21,22 +21,26 @@ public class ensyu4_12 {
 		// Scannerクラスのインスタンスを作成
 		Scanner standardInput = new Scanner(System.in);
 		
-		// カウントダウンを行う旨を表示する
+		// カウントアップを行う旨を表示する
 		System.out.println("カウントアップします。");
 		
 		// 入力された値を格納する変数
 		int integerValue;
 		
+		// 整数を入力させる処理を正の整数値が入力されるまで繰り返す
 		do {
 			// 正の整数値の入力を促す
 			System.out.print("正の整数値 : ");
 			// 入力された値をint型の変数"integerValue"に代入する
 			integerValue = standardInput.nextInt();
+		// "integerValue"が0以下（正の整数値ではない）間繰り返す
 		} while (integerValue <= 0);
 		
 		// for文を制御する変数"countUpNumber"0で初期化し"integerValue"になるまでインクリメントすることでカウントアップを実現させる
-		for(int countUpNumber = 0; countUpNumber <= integerValue; countUpNumber++)
-			System.out.println(countUpNumber); // "countUpNumber"を表示する
+		for(int countUpNumber = 0; countUpNumber <= integerValue; countUpNumber++) {
+			// "countUpNumber"を表示する
+			System.out.println(countUpNumber);
+		}
 		
 		// 改行文字を出力する
 		System.out.println(); 
